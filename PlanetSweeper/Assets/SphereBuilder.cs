f0ba55055ce6831cf3a7c1aded7656337508d38e
Richard Reho <rrtw93@gmail.com>
2021-09-22 19:14:40 +0200

    Initialize ShpereBuilder.

---
+++ PlanetSweeper/Assets/SphereBuilder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBuilder
+{
+
+    private static Vector3[] ConstuctIcosahedron(Vector3 origo, float radius)
+    {
+
+        return new Vector3[0];
+    }
+
+    private static Vector3[] ConstructPentagon(Vector3 origo, float radius)
+    {
+        return new Vector3[0];
+    }
+    
+}
